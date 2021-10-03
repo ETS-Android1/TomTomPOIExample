20aee38b84d802059fa6f6c1a1acbdcc69fafa9f
johnkozaris <ioanniskozaris@gmail.com>
2021-10-03 19:45:53 +0300

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text, TextInput,
+  useColorScheme,
+  View
+} from "react-native";
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+
+const App = () => {
+
+
+  return (
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
